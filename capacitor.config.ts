ab1f8f627112ae2943c5dd4addc2e13286fec36f
zaryab ali <zaryab.ali@clarisync.com>
2025-03-08 12:45:38 +0500

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.yourcompany.backgroundremover',
   appName: 'Background Remover',
   webDir: 'www',  // Ensure this is correct
+  plugins: {
+    LiveUpdates: {
+      appId: '995343ea',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 
 };
 
